a9f4fe07ec962c0197e65ef25dd5c4a96e55e324
hrithik125 <98795563+hrithik125@users.noreply.github.com>
2022-02-11 00:26:13 +0530

    Update source file

--- 3_Implementation/src/tictactoe.c
+++ 3_Implementation/src/tictactoe.c
@@ -1,4 +1,7 @@
-#include "tictactoe.h"
+#include"tic.h"
+
+
+
 int checkforwin()
 {
     if(a[0]==a[1] && a[1]==a[2])
@@ -23,8 +26,6 @@ int checkforwin()
         return -1;
 }
 
-
-
 void board()
 {
     int i;
@@ -46,19 +47,19 @@ void board()
     }
 void rules()
 {
-    char link;
+    char link='n';
     printf("\tTic-Tac-Toe\n\n");
     //printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
     printf("Rules:-\n");
     printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
     printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
     printf("\n\nEnjoy the game! Be a Winner!\n\n");
-    printf("To know more about the game press Y else type any other character:- ");
-    scanf("%c",&link);
-    // if(link=='y' || link=='Y')
-    // {
-    //     system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
-    // }
+    // printf("To know more about the game press Y else type any other character:- ");
+    // scanf("%c",&link);
+    if(link=='y' || link=='Y')
+    {
+        system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
+    }
 
 }
 int decision()
