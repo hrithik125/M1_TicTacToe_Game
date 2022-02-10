602cadab4827b79f25babfb66b4d30c28c7c26fb
hrithik125 <98795563+hrithik125@users.noreply.github.com>
2022-02-11 00:26:58 +0530

    Update header file

--- 3_Implementation/inc/tictactoe.h
+++ 3_Implementation/inc/tictactoe.h
@@ -1,15 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-//#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#ifndef TIC_H_
+#define TIC_H_
 
 char x,o;
 char a[9]={'1','2','3','4','5','6','7','8','9'};
 char u1[50],u2[50];
-void board();
-void rules();
-int checkforwin();
+
+
 
 void board();
 void rules();
 int checkforwin();
 int decision();
+
+
+
+
+#endif
