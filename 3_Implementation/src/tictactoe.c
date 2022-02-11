3f3c9ff12ddde4f7caf36360b0f52b658533155e
hrithik125 <contacthri89@gmail.com>
2022-02-11 22:53:42 +0530

    Added gitignore

--- 3_Implementation/src/tictactoe.c
+++ 3_Implementation/src/tictactoe.c
@@ -1,18 +1,16 @@
 #include"tictactoe.h"
 
-
-
 void rules()
 {
-    char link='n';
+    
     printf("\tTic-Tac-Toe\n\n");
-    //printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
+    
     printf("Rules:-\n");
     printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
     printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
     printf("\n\nEnjoy the game! Be a Winner!\n\n");
-    // printf("To know more about the game press Y else type any other character:- ");
-    // scanf("%c",&link);
+
+    char link='n';
     if(link=='y' || link=='Y')
     {
         system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
